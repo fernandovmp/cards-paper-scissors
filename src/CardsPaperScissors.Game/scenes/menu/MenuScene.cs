6dc88c32e972bc72228436e274c935b0c7494886
Fernando Mariano Pereira <fernandovmp@outlook.com>
2024-01-23 19:00:02 -0300

    adicionar créditos e configurar exportação

--- src/CardsPaperScissors.Game/scenes/menu/MenuScene.cs
+++ src/CardsPaperScissors.Game/scenes/menu/MenuScene.cs
@@ -7,11 +7,34 @@ namespace CardsPaperScissors.Game.scenes.Menu;
 public partial class MenuScene : CanvasLayer
 {
     private Button _startButton = default!;
+    private Button _creditsButton = default!;
+    private Control _titleRoot = default!;
+    private Control _creditsRoot = default!;
 
     public override void _Ready()
     {
-        _startButton = GetNode<Button>("Start");
+        _startButton = GetNode<Button>("Title/Start");
+        _creditsButton = GetNode<Button>("Title/Credits");
+        _titleRoot = GetNode<Control>("Title");
+        _creditsRoot = GetNode<Control>("Credits");
+        
         var root = GetNode(Constants.RootNode);
         _startButton.Pressed += () => SceneLoader.LoadInto(root, Constants.PreMatchScene);
+        _creditsButton.Pressed += () => ShowUi(title: false);
+        
+        using var file = FileAccess.Open("res://credits.txt", FileAccess.ModeFlags.Read);
+        var credits = file.GetAsText();
+        var label = _creditsRoot.GetNode<Label>("Label");
+        label.Text = credits;
+
+        _creditsRoot.GetNode<Button>("Back").Pressed += () => ShowUi(title: true);
+
+        ShowUi(title: true);
+    }
+
+    private void ShowUi(bool title)
+    {
+        _creditsRoot.Visible = !title;
+        _titleRoot.Visible = title;
     }
 }
